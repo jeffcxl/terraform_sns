50e6ead50d11cccea77a2f923dd7b40a21951c94
jeffcxl <jeffcxl@hotmail.com>
2024-07-20 11:12:18 +0800

    lint2

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 5.58.0"  # Adjust to the version you are using or need
+  required_version = ">= 1.0.0"  # Adjust to the version you are using or need
 }
 
 terraform {
